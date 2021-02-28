fddc482ec3cdb7ffb7c62ff69fec0a7d97f4ebaf
Andrew R <arrooney@ualberta.ca>
2021-02-28 10:14:16 -0700

    remove duplicate Trial class

--- app/src/main/java/com/T05/krowdtrialz/model/experiment/Experiment.java
+++ app/src/main/java/com/T05/krowdtrialz/model/experiment/Experiment.java
@@ -4,6 +4,7 @@ import com.T05.krowdtrialz.model.location.Region;
 import com.T05.krowdtrialz.model.scannable.Barcode;
 import com.T05.krowdtrialz.model.scannable.QRCode;
 import com.T05.krowdtrialz.model.user.User;
+import com.T05.krowdtrialz.model.trial.Trial;
 
 import java.util.ArrayList;
 import java.util.Collection;
