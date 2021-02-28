4d7b64f6c2dafe8c8606a96b4534c4eaf7455e4e
Furmaan <sekhon54321@gmail.com>
2021-02-27 20:16:39 -0700

    Changed user model class

--- app/src/main/java/com/T05/krowdtrialz/ui/User.java
+++ app/src/main/java/com/T05/krowdtrialz/ui/User.java
@@ -5,4 +5,21 @@ public class User {
     private String id;
     private String email;
 
+    public User(String name, String id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
