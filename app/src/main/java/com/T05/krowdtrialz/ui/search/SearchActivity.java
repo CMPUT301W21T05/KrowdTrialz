5465bb9d0d30701efe3de8b17cf2f915a24ad605
Andrew R <arrooney@ualberta.ca>
2021-03-07 22:41:02 -0700

    use searchView

--- app/src/main/java/com/T05/krowdtrialz/ui/search/SearchActivity.java
+++ app/src/main/java/com/T05/krowdtrialz/ui/search/SearchActivity.java
@@ -16,6 +16,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.T05.krowdtrialz.R;
 import com.T05.krowdtrialz.model.experiment.Experiment;
@@ -24,11 +25,13 @@ import com.T05.krowdtrialz.util.ExperimentList;
 
 import java.util.ArrayList;
 
-public class SearchActivity extends Activity {
+public class SearchActivity extends Activity implements SearchView.OnQueryTextListener{
 
     private Database db;
     private ArrayAdapter<Experiment> experimentAdapter;
     private final String TAG = "SearchActivity";
+    private SearchView searchExperimentsQuery;
+    private ListView searchResultsList;
 
     public SearchActivity() {
         super();
@@ -40,42 +43,44 @@ public class SearchActivity extends Activity {
         setContentView(R.layout.activity_search);
         db = new Database();
 
-        Button searchExperimentsButton = findViewById(R.id.search_experiments_button);
+        searchExperimentsQuery = findViewById(R.id.search_experiment_query);
 
-        ListView searchResultsList = findViewById(R.id.search_results_list);
-
-        EditText searchEditText = findViewById(R.id.search_editText);
+        searchResultsList = findViewById(R.id.search_results_list);
 
         experimentAdapter = new ExperimentList(this, new ArrayList<>());
 
         searchResultsList.setAdapter(experimentAdapter);
 
-        searchExperimentsButton.setOnClickListener(new View.OnClickListener() {
+        searchExperimentsQuery.setOnQueryTextListener(this);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        searchExperiments(query);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        searchExperiments(newText);
+        return true;
+    }
+
+    public void searchExperiments(String searchString) {
+        experimentAdapter.clear();
+
+        Log.e(TAG, "Query: " + searchString);
+        db.getExperimentsByDescription(searchString, new Database.QueryExperimentsCallback() {
             @Override
-            public void onClick(View v) {
-                String searchString = searchEditText.getText().toString();
-
+            public void onSuccess(ArrayList<Experiment> experiments) {
+                Log.d(TAG, "Got search results" + experiments.toString());
                 experimentAdapter.clear();
+                experimentAdapter.addAll(experiments);
+            }
 
-                if (searchString.isEmpty()) {
-                    Log.e(TAG, "No Query");
-                    return;
-                }
-
-                Log.e(TAG, searchString);
-                db.getExperimentsByDescription(searchString, new Database.QueryExperimentsCallback() {
-                    @Override
-                    public void onSuccess(ArrayList<Experiment> experiments) {
-                        Log.d(TAG, "Got search results" + experiments.toString());
-                        experimentAdapter.addAll(experiments);
-                        experimentAdapter.notifyDataSetChanged();
-                    }
-
-                    @Override
-                    public void onFailure() {
-                        Log.e(TAG, "Error Searching Database");
-                    }
-                });
+            @Override
+            public void onFailure() {
+                Log.e(TAG, "Error Searching Database");
             }
         });
     }
