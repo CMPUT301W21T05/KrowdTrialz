604fc4df1a658229b005036c2666adb6af314c77
Andrew R <arrooney@ualberta.ca>
2021-03-10 15:37:46 -0700

    fix test

--- app/src/test/java/com/T05/krowdtrialz/ExperimentTest.java
+++ app/src/test/java/com/T05/krowdtrialz/ExperimentTest.java
@@ -34,13 +34,13 @@ public class ExperimentTest {
         MeasurementExperiment experiment = mockMeasurementExperiment();
 
         experiment.setMinTrials(12);
-        experiment.setRegion("NOrth america");
+        experiment.setRegion("NOrth America");
 
         Set<String> tags = experiment.getTags();
         Set<String> expectedTags = new HashSet<>();
 
-        expectedTags.addAll(Arrays.asList("here", "joe", "12", "jbeast",
-                "test", "experiment", "bob", "jb@gmail.com", "cm", "measurement", "north", "america"));
+        expectedTags.addAll(Arrays.asList("joe", "12", "jbeast", "measurement",
+                "test", "experiment", "bob", "jb@gmail.com", "cm", "north", "america"));
 
         assertEquals(expectedTags, tags);
     }
