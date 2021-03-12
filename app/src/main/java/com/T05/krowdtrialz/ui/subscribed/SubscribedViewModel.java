3ca44ddf948e4b56d66e079216aea919e9792682
Vasu Gupta <punarvas@ualberta.ca>
2021-03-12 16:42:00 +0300

    implemented SubscribedViewModel

--- app/src/main/java/com/T05/krowdtrialz/ui/subscribed/SubscribedViewModel.java
+++ app/src/main/java/com/T05/krowdtrialz/ui/subscribed/SubscribedViewModel.java
@@ -1,5 +1,7 @@
 package com.T05.krowdtrialz.ui.subscribed;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -13,15 +15,27 @@ public class SubscribedViewModel extends ViewModel {
 
     private Database db;
     private MutableLiveData<ArrayList<Experiment>> experiments;
+    private static final String TAG = "SubscribedViewModel";
 
     public SubscribedViewModel() {
-        db = new Database();
+        db = Database.getInstance();
         experiments = new MutableLiveData<>();
     }
 
     public LiveData<ArrayList<Experiment>> getExperimentList(){
-        // TODO: implement this
-        //db.getExperimentsBySubscriber();
+        db.getExperimentsBySubscriber(db.getDeviceUser(), new Database.QueryExperimentsCallback() {
+            @Override
+            public void onSuccess(ArrayList<Experiment> experiments) {
+                experiments.clear();
+                experiments.addAll(experiments);
+                Log.d(TAG, "Got query results: " + experiments.toString());
+            }
+
+            @Override
+            public void onFailure() {
+                Log.e(TAG, "Error Querying database.");
+            }
+        });
         return experiments;
     }
 
