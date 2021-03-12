399c01abb278404483d9023648c128ec296b7e6b
Andrew R <arrooney@ualberta.ca>
2021-03-11 19:33:49 -0700

    use set in tagged interface

--- app/src/main/java/com/T05/krowdtrialz/model/interfaces/Tagged.java
+++ app/src/main/java/com/T05/krowdtrialz/model/interfaces/Tagged.java
@@ -1,11 +1,10 @@
 package com.T05.krowdtrialz.model.interfaces;
 
-import java.util.ArrayList;
 import java.util.Set;
 
 /**
  * Implementing classes maintain a set of tag strings
  */
 public interface Tagged {
-    public ArrayList<String> getTags();
+    public Set<String> getTags();
 }
