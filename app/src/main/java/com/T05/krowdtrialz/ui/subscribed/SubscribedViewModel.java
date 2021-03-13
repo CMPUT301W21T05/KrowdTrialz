d7c1b5af4d81d32c4a2ade6f8a66c5c6ced2add4
Vasu Gupta <punarvas@ualberta.ca>
2021-03-14 02:16:54 +0300

    fixes in SubscribedViewModel

--- app/src/main/java/com/T05/krowdtrialz/ui/subscribed/SubscribedViewModel.java
+++ app/src/main/java/com/T05/krowdtrialz/ui/subscribed/SubscribedViewModel.java
@@ -14,20 +14,20 @@ import java.util.ArrayList;
 public class SubscribedViewModel extends ViewModel {
 
     private Database db;
-    private MutableLiveData<ArrayList<Experiment>> experiments;
+    private MutableLiveData<ArrayList<Experiment>> experimentsList;
     private static final String TAG = "SubscribedViewModel";
 
     public SubscribedViewModel() {
         db = Database.getInstance();
-        experiments = new MutableLiveData<>();
+        experimentsList = new MutableLiveData<>();
     }
 
     public LiveData<ArrayList<Experiment>> getExperimentList(){
         db.getExperimentsBySubscriber(db.getDeviceUser(), new Database.QueryExperimentsCallback() {
             @Override
             public void onSuccess(ArrayList<Experiment> experiments) {
-                experiments.clear();
-                experiments.addAll(experiments);
+                experimentsList.getValue().clear();
+                experimentsList.getValue().addAll(experiments);
                 Log.d(TAG, "Got query results: " + experiments.toString());
             }
 
@@ -36,7 +36,7 @@ public class SubscribedViewModel extends ViewModel {
                 Log.e(TAG, "Error Querying database.");
             }
         });
-        return experiments;
+        return experimentsList;
     }
 
 }
